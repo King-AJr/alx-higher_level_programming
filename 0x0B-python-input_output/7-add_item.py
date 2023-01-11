86586602ae9a1b11ba8e069f8949c859b33ad642
King-AJr <talk2king.aj@gmail.com>
2023-01-11 09:54:54 +0100

    task 10

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,14 @@ import sys
 
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
     try:
-        items = load_from_json_file('add_items.json')
-        save_to_json_file(items, 'add_items.json')
+        items = load_from_json_file("add_items.json")
+        save_to_json_file(items, "add_items.json")
 
     except FileNotFoundError:
         items = []
         items.extend(sys.argv[1:])
-        save_to_json_file(items, 'add_items.json')
+        save_to_json_file(items, "add_items.json")
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,6 +4,7 @@
 
 class Student:
     """represents a student"""
+
     def __init__(self, first_name, last_name, age):
         """initialize new student"""
         self.first_name = first_name
