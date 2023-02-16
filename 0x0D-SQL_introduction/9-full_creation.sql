052e78f049be51471d0e977a505d87d8de4f929e
King-AJr <talk2king.aj@gmail.com>
2023-02-16 05:03:47 -0800

    add into command

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE table IF NOT EXISTS second_table(
 	score INT
 );
 
-INSERT `second_table` (id, name, score) VALUES(1, 'John', 10);
-INSERT `second_table` (id, name, score) VALUES(2, 'Alex' 3);
-INSERT `second_table` (id, name, score) VALUES(3, 'Bob' 14);
-INSERT `second_table` (id, name, score) VALUES(4, 'George', 8);
+INSERT INTO `second_table` (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO `second_table` (id, name, score) VALUES(2, 'Alex' 3);
+INSERT INTO `second_table` (id, name, score) VALUES(3, 'Bob' 14);
+INSERT INTO `second_table` (id, name, score) VALUES(4, 'George', 8);
