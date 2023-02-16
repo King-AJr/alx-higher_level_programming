a07510df28b07ff4b4ac96223b730ab57802a485
King-AJr <talk2king.aj@gmail.com>
2023-02-16 05:34:58 -0800

    pick only name nd score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- list all records from top top to bottom based on score
 
-SELECT `score` `name` FROM `second_table` ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
