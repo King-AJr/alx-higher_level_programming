a40620d685a45775d916f9032fc8f0cb39ea3bed
King-AJr <talk2king.aj@gmail.com>
2023-03-23 05:32:34 -0700

    use format to add variable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s"
-    state = sys.argv[4]
-    cur.execute(query, (state,))
+    query = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
