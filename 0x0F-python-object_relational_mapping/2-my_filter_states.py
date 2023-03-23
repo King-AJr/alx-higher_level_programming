5acd501000e0a417c477f531db868d9d2a0a11f6
King-AJr <talk2king.aj@gmail.com>
2023-03-23 05:19:34 -0700

    display all values in the states where name matches argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+display all values in the states where name matches an arguent
 # Usage: ./1-filter_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
+                              <state name searched>   
 """
 
 import sys
@@ -12,7 +13,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name LIKE 'n%'")
+    query = "SELECT * FROM states WHERE name = %s"
+    state = sys.argv[4]
+    cur.execute(query, (state,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
