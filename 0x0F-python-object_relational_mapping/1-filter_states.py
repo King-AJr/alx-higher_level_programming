c7f584fe2544b1595c8b27eae6cf2c575b06ce3c
King-AJr <talk2king.aj@gmail.com>
2023-03-23 04:52:33 -0700

    set docstring

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-# lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+"""
 
 import sys
 import MySQLdb
