a39ebb4a05b0c6ac0b2ecdde963b7fa8d331f727
King-AJr <talk2king.aj@gmail.com>
2023-04-04 06:02:54 -0700

    document module and stop execution when imported

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" takes in url, sends request and
+"""takes in url, sends request and
     displays request id
 """
 
 import requests
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     print(response.headers['X-Request-Id'])
