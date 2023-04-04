b2208ea8707d5913a3b2ae9ab53a61e951289e5e
King-AJr <talk2king.aj@gmail.com>
2023-04-04 09:03:41 -0700

    take github user name and repo then prints out authors for the last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    print(url)
+    headers = {
+            'Authorization': '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'
+            }
+    params = {'author': sys.argv[2], 'Authorization': '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'}
+
+    response = requests.get(url, headers=headers)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(commits[i].get("sha"), commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        print("", end="")
