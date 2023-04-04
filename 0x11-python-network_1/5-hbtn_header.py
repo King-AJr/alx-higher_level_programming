4f942578b8a355b8dfaf5b158df174dbb7813df0
King-AJr <talk2king.aj@gmail.com>
2023-04-04 05:55:17 -0700

    print request id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,5 +6,6 @@
 import requests
 import sys
 
-response = requests.get(sys.argv[1])
-print(response.headers['X-Request-Id'])
+if __name__ = "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])
