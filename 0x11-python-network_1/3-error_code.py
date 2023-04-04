499e42ddf4a9a1f2d650c990b1e2484a2c406a79
King-AJr <talk2king.aj@gmail.com>
2023-04-04 04:44:26 -0700

    take url, send request, display response body and manage errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""takes a url, displays body of response
+    also manages urllib and http errors
+"""
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urlopen(sys.argv[1]) as response:
+            html = response.read()
+    except HTTPError as e:
+        print('Error code: ', e.code)
+    except URLError as e:
+        print('Error code: ', e.code)
+    else:
+        print('{}'.format(html.decode("utf-8")))
