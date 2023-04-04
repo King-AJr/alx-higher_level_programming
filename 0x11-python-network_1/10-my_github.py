d33be6f0d2ec766ce9c60516b98168425a52c134
King-AJr <talk2king.aj@gmail.com>
2023-04-04 08:15:20 -0700

    stops file from executing when imported

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,9 +7,10 @@ and uses the GitHub API to display your id
 import requests
 import sys
 
-url = 'https://api.github.com/user'
-auth = (sys.argv[1], sys.argv[2])
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    auth = (sys.argv[1], sys.argv[2])
 
-response = requests.get(url, auth=auth)
-res = response.json()
-print(res.get("id"))
+    response = requests.get(url, auth=auth)
+    res = response.json()
+    print(res.get("id"))
