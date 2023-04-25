bf0e704e5109953c7c384a58ef0c1fd9dc96bea2
King-AJr <talk2king.aj@gmail.com>
2023-04-25 19:26:32 +0000

    append data to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* writing data to a file */
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const filename = args[0];
+const data = args[1];
+
+fs.appendFile(filename, data, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
+
