24e76bdf3fa3734fd798f5e5f306bedf8518c432
King-AJr <talk2king.aj@gmail.com>
2023-04-25 21:32:55 +0000

    gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* gets the contents of a webpage and stores it in a file.*/
+const fs = require('fs');
+const request = require('request');
+
+const args = process.argv.slice(2);
+request(args[0]).pipe(fs.createWriteStream(args[1]));
