95c232e07cc1a58f46a8c1d019b846422ed6f3fb
King-AJr <talk2king.aj@gmail.com>
2023-04-25 19:05:48 +0000

    take filename and open if available

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* get file name and print file content */
+const fs = require('fs');
+const args = process.argv.slice(2);
+const filename = args[0];
+
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+});
